7a25d5629a80b14058bbcc12a154b6547ff00a82
seoandx <seoandx@yahoo.co.jp>
2021-05-16 21:53:19 +0900

    ロト当選番号リスト修正 ①検索機能追加 ②リストデザイン修正

--- LotoAutoNumber/Controller/LotoListController.swift
+++ LotoAutoNumber/Controller/LotoListController.swift
@@ -7,16 +7,23 @@
 
 import UIKit
 
-class LotoListController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class LotoListController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     // MARK: - Properties
+    
+    // ロト当選番号リスト生成
     var tableView: UITableView?
+    
+    // ロト当選回検索コントロール生成
+    var searchBar: UISearchBar?
     
     // ロト6当選番号リストViewの親Viewのインスタンス生成
     private let containerView = UIView()
     
     // ロトリストモデル取得
     var lotolists = [LotoList]()
+    
+    var searchResults = [LotoList]()
     
     // MARK: - Lifecycle
     
@@ -74,25 +81,76 @@ class LotoListController: UIViewController, UITableViewDelegate, UITableViewData
     
     // テーブルView行数設定
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.lotolists.count
+        if searchBar!.text != "" {
+            return searchResults.count
+        }
+        else{
+            return self.lotolists.count
+        }
     }
     
+    // テーブルView表示データ設定
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") ?? UITableViewCell(style: .default, reuseIdentifier: "Cell")
-        let title = "当選番号 回号 : 第" + self.lotolists[indexPath.row].list_number + "回\n当選日 : " + self.lotolists[indexPath.row].list_date + "\n本番号 : "
-        let number1 = self.lotolists[indexPath.row].number1
-        let number2 = self.lotolists[indexPath.row].number2
-        let number3 = self.lotolists[indexPath.row].number3
-        let number4 = self.lotolists[indexPath.row].number4
-        let number5 = self.lotolists[indexPath.row].number5
-        let number6 = self.lotolists[indexPath.row].number6
-        let bouns = "\nボーナス番号 : " + self.lotolists[indexPath.row].bonus
-        cell.textLabel?.text = title + number1 + "," + number2 + "," + number3 + "," + number4 + "," + number5 + "," + number6 + bouns
         cell.textLabel?.numberOfLines = 4
         cell.textLabel?.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         cell.contentView.backgroundColor = #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1)
         
-        return cell
+        cell.separatorInset = .zero
+        tableView.separatorColor = .white
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 13)
+        
+        if searchBar!.text != "" {
+            let title = "当選番号 回号 : 第" + self.searchResults[indexPath.row].list_number + "回\n当選日 : " + self.searchResults[indexPath.row].list_date + "\n本番号 : "
+            let number1 = self.searchResults[indexPath.row].number1
+            let number2 = self.searchResults[indexPath.row].number2
+            let number3 = self.searchResults[indexPath.row].number3
+            let number4 = self.searchResults[indexPath.row].number4
+            let number5 = self.searchResults[indexPath.row].number5
+            let number6 = self.searchResults[indexPath.row].number6
+            let bouns = "\nボーナス番号 : " + self.searchResults[indexPath.row].bonus
+            cell.textLabel?.text = title + number1 + "," + number2 + "," + number3 + "," + number4 + "," + number5 + "," + number6 + bouns
+
+            return cell
+        }
+        else{
+            let title = "当選番号 回号 : 第" + self.lotolists[indexPath.row].list_number + "回\n当選日 : " + self.lotolists[indexPath.row].list_date + "\n本番号 : "
+            let number1 = self.lotolists[indexPath.row].number1
+            let number2 = self.lotolists[indexPath.row].number2
+            let number3 = self.lotolists[indexPath.row].number3
+            let number4 = self.lotolists[indexPath.row].number4
+            let number5 = self.lotolists[indexPath.row].number5
+            let number6 = self.lotolists[indexPath.row].number6
+            let bouns = "\nボーナス番号 : " + self.lotolists[indexPath.row].bonus
+            cell.textLabel?.text = title + number1 + "," + number2 + "," + number3 + "," + number4 + "," + number5 + "," + number6 + bouns
+
+            return cell
+        }
+    }
+    
+    // 検索ボタンが押された時に呼ばれる
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.view.endEditing(true)
+        searchBar.showsCancelButton = true
+        self.searchResults = self.lotolists.filter{
+            // ロト当選回検索
+            $0.list_number == self.searchBar!.text!
+        }
+        self.tableView!.reloadData()
+    }
+
+    // キャンセルボタンが押された時に呼ばれる
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        self.view.endEditing(true)
+        searchBar.text = ""
+        self.tableView!.reloadData()
+    }
+
+    // テキストフィールド入力開始前に呼ばれる
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.showsCancelButton = true
+        return true
     }
     
     func configureUI()
@@ -105,16 +163,38 @@ class LotoListController: UIViewController, UITableViewDelegate, UITableViewData
         containerView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
         containerView.backgroundColor = #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1)
         
-        // コンテイナViewにログインView追加
+        //当選番号検索コントロール追加
+        self.searchBar = {
+           let searchBar = UISearchBar()
+            searchBar.delegate = self
+            searchBar.frame = CGRect(x:0, y:0, width:self.view.frame.width, height:42)
+            searchBar.layer.position = CGPoint(x: self.view.bounds.width/2, y: 89)
+            searchBar.searchBarStyle = UISearchBar.Style.default
+            searchBar.showsSearchResultsButton = true
+            searchBar.placeholder = "検索"
+            searchBar.setValue("キャンセル", forKey: "cancelButtonText")
+            //キャンセルボタン色
+            searchBar.tintColor = .white
+            //背景色変更
+            searchBar.barTintColor = #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1)
+            //テキスト色
+            searchBar.searchTextField.textColor = .white
+            
+            return searchBar
+        }()
+        
+        // コンテイナViewにロト当選番号リストView追加
         self.tableView = {
             let tableView = UITableView(frame: self.view.bounds, style: .plain)
-            tableView.autoresizingMask = [
-              .flexibleWidth,
-              .flexibleHeight
-            ]
+                tableView.autoresizingMask = [
+                    .flexibleWidth,
+                    .flexibleHeight
+                ]
             containerView.addSubview(tableView)
-            tableView.frame = CGRect(x: containerView.bounds.origin.x, y: containerView.frame.origin.y,
-                                     width: containerView.bounds.width, height: containerView.bounds.height)
+            tableView.frame = CGRect(x: containerView.bounds.origin.x,
+                                     y: containerView.frame.origin.y,
+                                     width: containerView.bounds.width,
+                                     height: containerView.bounds.height)
             
             tableView.delegate = self
             tableView.dataSource = self
@@ -123,9 +203,11 @@ class LotoListController: UIViewController, UITableViewDelegate, UITableViewData
             tableView.backgroundColor = #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1)
             tableView.rowHeight = UITableView.automaticDimension
             
+            tableView.tableHeaderView = searchBar
+            
             return tableView
 
           }()
-
+    
     }
 }
